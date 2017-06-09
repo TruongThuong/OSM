b8fee91336a5754dab5e970865eb966d5002891e
ThuongTruong <thuongtruong2702@gmail.com>
2017-06-09 10:39:07 +0700

    Add models

---
+++ OSM.Model/Entities/Error.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OSM.Model.Entities
+{
+    [Table("Errors")]
+    public class Error
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public string Message { get; set; }
+
+        public string StackTrace { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
